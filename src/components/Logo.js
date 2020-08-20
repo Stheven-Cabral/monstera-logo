e30d8c510c31c0e4d3a9ce3d455ca0b1a7bf03cd
Stheven Cabral <stheven.cabral@gmail.com>
2020-08-19 21:52:01 -0700

    Experimenting with colord.

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Logo = () => {
   return(
     <div className="inner-container">
-      <h1>MONSTERA</h1>
-      <h3>Deliciosa</h3>
+      {/* <h1>MONSTERA</h1>
+      <h3>Deliciosa</h3> */}
     </div>
   )
 }
